b9ec544882fd136c8bfc9060885d531131c9268e
sophiacharlize <league@WorthyMini-6.attlocal.net>
2018-02-18 12:55:26 -0800

    hi
    ih

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,31 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+	public static double divide(double a, double b) {
+		if (b == 0) {
+			throw new IllegalArgumentException();
+		}
+		return a / b;
+	}
+
+	public static String reverseString(String a) {
+		System.out.println("started");
+		String temp = "";
+		System.out.println();
+		if (a.equals("")) {
+			throw new IllegalStateException();
+		} else {
+			System.out.println("else");
+			for (int i = a.length()-1; i >= 0; i--) {
+				temp = temp + a.charAt(i);
+				System.out.println(temp);
+			}
+
+			System.out.println(temp);
+			
+			return temp;
+		}
+
+	}
+
 }
--- src/_02_gotta_catchem_all/ExceptionsTest.java
+++ src/_02_gotta_catchem_all/ExceptionsTest.java
@@ -10,29 +10,29 @@ class ExceptionsTest {
 	
 	//1. This is an example of how one might test that an exception is thrown.
 	//   It tests that an IndexOutOfBoundsException is thrown 
-	@Test
-	public void testIndexOOBException() {
-		int[] vars = {0, 1, 2, 3};
-			
-		try {
-			vars[0] = 4;
-			assertArrayEquals(vars, new int[] {4, 1, 2, 3});
-			vars[4] = 5;
-			fail("IndexOutOfBoundsException not thrown");
-		}catch(IndexOutOfBoundsException e) {
-			e.printStackTrace();
-			assertArrayEquals(vars, new int[] {4, 1, 2, 3});
-		}
-	}
+//	@Test
+//	public void testIndexOOBException() {
+//		int[] vars = {0, 1, 2, 3};
+//			
+//		try {
+//			vars[0] = 4;
+//			assertArrayEquals(vars, new int[] {4, 1, 2, 3});
+//			vars[4] = 5;
+//			fail("IndexOutOfBoundsException not thrown");
+//		}catch(IndexOutOfBoundsException e) {
+//			e.printStackTrace();
+//			assertArrayEquals(vars, new int[] {4, 1, 2, 3});
+//		}
+//	}
 	
 	//2. In the ExceptionMethods class, write a method called divide that takes
-	//   two doubles as parameters and returns their product as a double. This method 
+	//   two doubles as parameters and returns their magikarp as a double. This method 
 	//   should throw an IllegalArgumentException if the denominator is 0.0.
 	
 	//3. Complete the JUnit test method to test the divide method.
 	@Test
 	public void testDivideByZeroException() {
-		
+		assertEquals(ExceptionMethods.divide(2,2),1);
 	}
 	
 	//4. In the ExceptionMethods class, write a method called reverseString that takes a
@@ -40,9 +40,11 @@ class ExceptionsTest {
 	//   if the String passed in is empty
 	
 	//5. Complte the JUnit test method to test the reverseStringMethod.
+	
 	@Test
-	public void testReverseString(String s) {
-		
+	public void testReverseString() {
+		System.out.println("weeeeeeeeeeeeee?");
+		assertEquals(ExceptionMethods.reverseString("hi"),"ih");
 	}
 	
 	
